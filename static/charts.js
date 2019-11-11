18a587a68941f5d0d7ae5b0310c2c218a3414d76
PhoeniXuzoo <PhoeniXuzoo@hotmail.com>
2019-11-11 11:10:55 -0600

    modify charts.html&static

--- static/charts.js
+++ static/charts.js
@@ -1,3 +1,4 @@
+/*
 var LineChart = echarts.init(document.getElementById('LineChart'));
 var Pie = echarts.init(document.getElementById('Pie'));
 
@@ -89,4 +90,43 @@ var PieOption = {
 };
 
 LineChart.setOption(LineChartOption);
+window.addEventListener("resize", function () {
+    LineChart.resize();
+});
 Pie.setOption(PieOption);
+window.addEventListener("resize", function () {
+    Pie.resize();
+});*/
+
+var Chart = echarts.init(document.getElementById('Chart'));
+
+var ChartOption = {
+    title: {
+        text: ''
+    },
+    toolbox: {
+        show : true,
+        feature : {
+            mark : {show: true},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        center : ['50%', '50%'],
+        data: [820, 932, 901, 934, 1290, 1330, 1320, 693, 788, 1573, 973, 1160],
+        type: 'line'
+    }]
+};
+
+Chart.setOption(ChartOption);
+window.addEventListener("resize", function () {
+    Chart.resize();
+});
